d779de83f910df1dfcca3ac4d9b8bd082a64507c
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2024-04-10 20:52:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // complete this js code
-function Person(name, age) {}
+class Person(name, age) {
+	constructor(name , age){
+		this.name = name;
+		this.age = age;
+	}
+	greet(){
+		return `Hello, my name is ${this.name} , I am ${this.age} years old.`
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+class Employee extends Person (name, age, jobTitle) {
+	constructor(jobTitle){
+		super(name , age);
+		this.jobTitle = jobTitle;
+		
+	}
+	jobGreet(){
+		return `Hello, my name is ${this.name}, I am ${this.age} years old , and my job title is ${this.jobTitle}`
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
