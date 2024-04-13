4103bd4563478c943b7f5bd42b4e49b4ae8d1aa3
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2024-04-13 11:55:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-function Person(name , age) {
-	this.name = name;
-	this.age = age;
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
 }
-Person.prototype.greet = function() {
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-	
+
+Person.prototype.greet = function () {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 };
+
 function Employee(name, age, jobTitle) {
-	Person.call(this , name , age);
-	this.jobTitle = jobTitle;
+    Person.call(this, name, age);
+    this.jobTitle = jobTitle;
 }
+
 Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee; 
+Employee.prototype.constructor = Employee;
+
 Employee.prototype.jobGreet = function () {
-	console.log(`Hello, my name is ${this.name}, I am ${this.years} old, and my job title is ${this.jobTitle}`);
-	
-}
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
+
+// Export the constructors
 window.Person = Person;
 window.Employee = Employee;
