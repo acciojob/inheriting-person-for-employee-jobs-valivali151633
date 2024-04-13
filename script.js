95d100d509cd623ec0b38fa6fd62e2717f480c47
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2024-04-13 11:49:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  greet() {
-    console.log(`Hello my name is ${this.name}, I am ${this.age} years old.`);
-  }
+function Person(name , age) {
+	this.name = name;
+	this.age = age;
 }
-
-class Employee extends Person {
-  constructor(name, age, jobTitle) {
-    super(name, age);
-    this.jobTitle = jobTitle;
-  }
-
-  jobGreet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-  }
+Person.prototype.greet = function() {
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+	
+};
+function Employee(name, age, jobTitle) {
+	Person.call(this , name , age);
+	this.jobTitle = jobTitle;
 }
-
-// Output: Hello, my name is Bob, I am 30 years old, and my job title is Manager.
-
-// Do not change code below this line
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee; 
+Employee.prototype.jobGreet = function () {
+	console.log(`Hello, my name is ${this.name}, I am ${this.years} old, and my job title is ${this.jobTitle}`);
+	
+}
 window.Person = Person;
 window.Employee = Employee;
